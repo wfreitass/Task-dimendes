45fd9ca0e12020e3aa618cf71f98d3f74650423c
Wiltter Freitas <wiltterfreitasw@gmail.com>
2023-06-10 07:29:25 -0300

    login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,11 +17,11 @@
                                 <div class="col-md-6">
                                     <input id="name" type="text"
                                         class="form-control @error('name') is-invalid @enderror" name="name"
-                                        value="{{ old('name') }}" autocomplete="name" autofocus>
+                                        value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                     @error('name')
                                         <span class="invalid-feedback" role="alert">
-                                            <strong>Campo Obrigatório</strong>
+                                            <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
                                 </div>
@@ -34,11 +34,11 @@
                                 <div class="col-md-6">
                                     <input id="email" type="email"
                                         class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" autocomplete="email">
+                                        value="{{ old('email') }}" required autocomplete="email">
 
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
-                                            <strong>Campo Obrigatório</strong>
+                                            <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
                                 </div>
@@ -51,11 +51,11 @@
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
-                                        autocomplete="new-password">
+                                        required autocomplete="new-password">
 
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
-                                            <strong>Campo Obrigatório</strong>
+                                            <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
                                 </div>
@@ -66,21 +66,15 @@
                                     class="col-md-4 col-form-label text-md-end">{{ __('Confirmar Senha') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password-confirm" type="password"
-                                        class="form-control @error('password_confirmation') is-invalid @enderror"
-                                        name="password_confirmation" autocomplete="new-password">
-                                    @error('password_confirmation')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>Campo Obrigatório</strong>
-                                        </span>
-                                    @enderror
+                                    <input id="password-confirm" type="password" class="form-control"
+                                        name="password_confirmation" required autocomplete="new-password">
                                 </div>
                             </div>
 
                             <div class="row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Registrar') }}
+                                        {{ __('Register') }}
                                     </button>
                                 </div>
                             </div>
